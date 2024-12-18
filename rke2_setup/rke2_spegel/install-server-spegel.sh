b7e875ebc7b33a842daf17f8c89b6387e9d66b1f
thomasferrandiz <thomas.ferrandiz@suse.com>
2024-12-18 14:58:35 +0100

    add option for airgap

--- rke2_setup/rke2_spegel/install-server-spegel.sh
+++ rke2_setup/rke2_spegel/install-server-spegel.sh
@@ -1,13 +1,45 @@
 #!/bin/bash -xv
 set -Eeuo pipefail
 
-#INSTALL_RKE2_VERSION=v1.28.4+rke2r1
-#INSTALL_RKE2_VERSION=v1.29.0-rc1+rke2r1
-INSTALL_RKE2_VERSION="latest"
+USE_AIRGAP=1
+
+INSTALL_RKE2_VERSION=v1.30.7+rke2r1
+# INSTALL_RKE2_VERSION="latest"
 INSTALL_RKE2_TYPE="server"
+
+JOIN="${JOIN:-0}"
+
+OPTIND=1  
+while getopts "h?vja" opt; do
+    case "$opt" in
+    h|\?)
+        echo "Usage: $0 [-v] [-j] [-a]"
+        exit 0
+        ;;
+    v)  verbose=1
+        ;;
+    j)  JOIN=$OPTARG
+        ;;
+    a)  USE_AIRGAP=$OPTARG
+        ;;
+
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
 
 # uninstall
 /usr/local/bin/rke2/bin/rke2-uninstall.sh || true
+
+#airgap install
+if [[ $USE_AIRGAP != 0 ]]; then
+    IMAGES_DIR=/var/lib/rancher/rke2/agent/images/
+    sudo mkdir -p ${IMAGES_DIR}
+    wget https://github.com/rancher/rke2/releases/download/${INSTALL_RKE2_VERSION}/rke2-images.linux-amd64.tar.gz
+    sudo mv ./rke2-images.linux-amd64.tar.gz ${IMAGES_DIR}
+fi
 
 FILE="/etc/rancher/rke2/registries.yaml"
 sudo mkdir -p $(dirname $FILE) 
@@ -15,7 +47,11 @@ sudo cp $PWD/registries.yaml $FILE
 
 FILE="/etc/rancher/rke2/config.yaml"
 sudo mkdir -p $(dirname $FILE) 
-sudo cp $PWD/config_server.yaml $FILE
+if [[ $JOIN == 0]]; then
+    sudo cp $PWD/config_server.yaml $FILE
+else
+    sudo cp $PWD/config_server_secondary.yaml $FILE
+fi
 
 curl -sfL https://get.rke2.io | INSTALL_RKE2_TYPE=${INSTALL_RKE2_TYPE} INSTALL_RKE2_CHANNEL=${INSTALL_RKE2_VERSION} sudo sh -
 
