e325e5df7c506ab57e6551a2b73a5af4dedad946
thomasferrandiz <thomas.ferrandiz@suse.com>
2025-02-21 12:08:41 +0100

    use opentofu instead of terraform

--- opentofu/infraup.sh
+++ opentofu/infraup.sh
@@ -10,11 +10,11 @@ case $1 in
   "azure")
     case $2 in
       "HA")
-        ip0=$(terraform output -json | jq '.ipAddresses.value[0]')
-        ip1=$(terraform output -json | jq '.ipAddresses.value[1]')
-        ip2=$(terraform output -json | jq '.ipAddresses.value[2]')
-        ip3=$(terraform output -json | jq '.ipAddresses.value[3]')
-        ip4=$(terraform output -json | jq '.ipAddresses.value[4]')
+        ip0=$(tofu output -json | jq '.ipAddresses.value[0]')
+        ip1=$(tofu output -json | jq '.ipAddresses.value[1]')
+        ip2=$(tofu output -json | jq '.ipAddresses.value[2]')
+        ip3=$(tofu output -json | jq '.ipAddresses.value[3]')
+        ip4=$(tofu output -json | jq '.ipAddresses.value[4]')
         echo $ip0
         echo $ip1
         echo $ip2
@@ -27,9 +27,9 @@ case $1 in
         sed -i '/^Host azure-ubuntu5/{n;s/Hostname .*/Hostname '$ip4'/}' ~/.ssh/config
       ;;
       *)
-        ip0=$(terraform output -json | jq '.ipAddresses.value[0]')
-        ip1=$(terraform output -json | jq '.ipAddresses.value[1]')
-        ip2=$(terraform output -json | jq '.ipAddresses.value[2]')
+        ip0=$(tofu output -json | jq '.ipAddresses.value[0]')
+        ip1=$(tofu output -json | jq '.ipAddresses.value[1]')
+        ip2=$(tofu output -json | jq '.ipAddresses.value[2]')
         sed -i '/^Host azure-ubuntu/{n;s/Hostname .*/Hostname '$ip0'/}' ~/.ssh/config
         sed -i '/^Host azure-ubuntu2/{n;s/Hostname .*/Hostname '$ip1'/}' ~/.ssh/config
         sed -i '/^Host azure-ubuntu3/{n;s/Hostname .*/Hostname '$ip2'/}' ~/.ssh/config
@@ -37,13 +37,13 @@ case $1 in
     esac
   ;;
   "aws")
-    ipv6=$(terraform output -json | jq '.ipv6IP.value[0]')
-    ipv4jump=$(terraform output -json | jq '.publicIP.value')
-    ipv4public1=$(terraform output -json | jq '.publicIP.value[0]')
-    ipv4public2=$(terraform output -json | jq '.publicIP.value[1]')
-    ipv4public3=$(terraform output -json | jq '.publicIP.value[2]')
-    ipv4public4=$(terraform output -json | jq '.publicIP.value[3]')
-    ipv4public5=$(terraform output -json | jq '.publicIP.value[4]')
+    ipv6=$(tofu output -json | jq '.ipv6IP.value[0]')
+    ipv4jump=$(tofu output -json | jq '.publicIP.value')
+    ipv4public1=$(tofu output -json | jq '.publicIP.value[0]')
+    ipv4public2=$(tofu output -json | jq '.publicIP.value[1]')
+    ipv4public3=$(tofu output -json | jq '.publicIP.value[2]')
+    ipv4public4=$(tofu output -json | jq '.publicIP.value[3]')
+    ipv4public5=$(tofu output -json | jq '.publicIP.value[4]')
     sed -i '/^Host aws-ubuntu/{n;s/Hostname .*/Hostname '$ipv4public1'/}' ~/.ssh/config
     sed -i '/^Host aws-ubuntu2/{n;s/Hostname .*/Hostname '$ipv4public2'/}' ~/.ssh/config
     sed -i '/^Host aws-ubuntu3/{n;s/Hostname .*/Hostname '$ipv4public3'/}' ~/.ssh/config
@@ -62,10 +62,10 @@ esac
 # updaterke1cluster updates the addresses of the rke1 cluster
 updaterke1cluster() {
   pushd $1
-  ipPublic0=$(terraform output -json | jq '.ipAddresses.value[0]')
-  ipPublic1=$(terraform output -json | jq '.ipAddresses.value[1]')
-  ipPrivate0=$(terraform output -json | jq '.ipPrivateAddresses.value[0]')
-  ipPrivate1=$(terraform output -json | jq '.ipPrivateAddresses.value[1]')
+  ipPublic0=$(tofu output -json | jq '.ipAddresses.value[0]')
+  ipPublic1=$(tofu output -json | jq '.ipAddresses.value[1]')
+  ipPrivate0=$(tofu output -json | jq '.ipPrivateAddresses.value[0]')
+  ipPrivate1=$(tofu output -json | jq '.ipPrivateAddresses.value[1]')
   sed -i '4s/.*/- address: '${ipPublic0}'/' ${RKECLUSTERFILE}
   sed -i '5s/.*/  internal_address: '${ipPrivate0}'/' ${RKECLUSTERFILE}
   sed -i '12s/.*/- address: '${ipPublic1}'/' ${RKECLUSTERFILE}
@@ -74,12 +74,12 @@ updaterke1cluster() {
   popd
 }
 
-# applyTerraform runs terraform apply and refresh to get the publicIP of the new VMs
-applyTerraform () {
+# applyTofu runs tofu apply and refresh to get the publicIP of the new VMs
+applyTofu () {
 pushd $1
-terraform apply --auto-approve
+tofu apply --auto-approve
 sleep 10
-terraform refresh
+tofu refresh
 sleep 5
 changeSshConfig $1 $2
 popd
@@ -92,7 +92,7 @@ case $1 in
     cp azure/template/azure.tf.template azure/azure.tf
     sed -i 's/%CLOUDINIT%/"..\/cloud-init-scripts\/installDockerHelm.sh"/g' azure/azure.tf
     sed -i 's/%COUNT%/2/g' azure/azure.tf
-    applyTerraform azure
+    applyTofu azure
     updaterke1cluster azure
   ;;
   "rancher")
@@ -100,7 +100,7 @@ case $1 in
     cp azure/template/azure.tf.template azure/azure.tf
     sed -i 's/%CLOUDINIT%/"..\/cloud-init-scripts\/installK3sAndRancher_${count.index}.sh"/g' azure/azure.tf
     sed -i 's/%COUNT%/2/g' azure/azure.tf
-    applyTerraform azure
+    applyTofu azure
     echo "Access ${ip0//\"/}.sslip.io in your browser"
   ;;
   "rancher-aws")
@@ -108,7 +108,7 @@ case $1 in
     cp aws/template/aws.tf.template aws/aws.tf
     sed -i 's/%CLOUDINIT%/"..\/cloud-init-scripts\/installK3sAndRancher_${count.index}.sh"/g' aws/aws.tf
     sed -i 's/%COUNT%/2/g' aws/aws.tf
-    applyTerraform aws
+    applyTofu aws
     echo "Access ${ipv4public1//\"/}.sslip.io in your browser"
   ;;
   "rancher-prime")
@@ -116,7 +116,7 @@ case $1 in
     cp azure/template/azure.tf.template azure/azure.tf
     sed -i 's/%CLOUDINIT%/"..\/cloud-init-scripts\/installK3sAndRancherPrime_${count.index}.sh"/g' azure/azure.tf
     sed -i 's/%COUNT%/2/g' azure/azure.tf
-    applyTerraform azure
+    applyTofu azure
     echo "Access ${ip0//\"/}.sslip.io in your browser"
   ;;
   "rancher-prime-aws")
@@ -124,7 +124,7 @@ case $1 in
     cp aws/template/aws.tf.template aws/aws.tf
     sed -i 's/%CLOUDINIT%/"..\/cloud-init-scripts\/installK3sAndRancherPrime_${count.index}.sh"/g' aws/aws.tf
     sed -i 's/%COUNT%/2/g' aws/aws.tf
-    applyTerraform aws
+    applyTofu aws
     echo "Access ${ipv4public1//\"/}.sslip.io in your browser"
   ;;
   "k3s")
@@ -132,20 +132,20 @@ case $1 in
     cp azure/template/azure.tf.template azure/azure.tf
     sed -i 's/%CLOUDINIT%/"..\/cloud-init-scripts\/installK3s_${count.index}.sh"/g' azure/azure.tf
     sed -i 's/%COUNT%/3/g' azure/azure.tf
-    applyTerraform azure
+    applyTofu azure
   ;;
   "k3s-aws")
     echo "k3s option"
     cp aws/template/aws.tf.template aws/aws.tf
     sed -i 's/%CLOUDINIT%/"..\/cloud-init-scripts\/installK3s_${count.index}.sh"/g' aws/aws.tf
     sed -i 's/%COUNT%/3/g' aws/aws.tf
-    applyTerraform aws
+    applyTofu aws
   ;;
   "k3s-ipv6")
     echo "k3s-ipv6 option"
     cp aws/template/aws.tf.template aws/aws.tf
     sed -i 's/%CLOUDINIT%/"..\/cloud-init-scripts\/installK3snoDS.sh"/g' aws/aws.tf
-    applyTerraform aws
+    applyTofu aws
   ;;
   "rke2")
     echo "rke2 option with cni plugin $2"
@@ -179,7 +179,7 @@ case $1 in
     cp aws/template/aws.tf.template aws/aws.tf
     sed -i 's/%CLOUDINIT%/"..\/cloud-init-scripts\/installRKE2_${count.index}.sh"/g' aws/aws.tf
     sed -i 's/%COUNT%/2/g' aws/aws.tf
-    applyTerraform aws
+    applyTofu aws
   ;;
   "windows")
     echo "rke2 and windows with cni plugin $2"
@@ -200,7 +200,7 @@ case $1 in
     cp azure/template/azure.tf.windows.template azure/azure.tf
     sed -i "s/cni: .*/cni: ${cniPlugin}/g" cloud-init-scripts\/installRKE2NoDS_0.sh
     sed -i 's/%CLOUDINIT%/"..\/cloud-init-scripts\/installRKE2NoDS_${count.index}.sh"/g' azure/azure.tf
-    applyTerraform azure
+    applyTofu azure
     echo "ssh azure-windows 'powershell.exe -File C:\AzureData\install.ps1 MYIP'"
     echo "ssh azure-windows 'powershell.exe C:\usr\local\bin\rke2.exe agent service --add'"
     echo "ssh azure-windows 'powershell.exe Start-Service -Name rke2'"
@@ -210,12 +210,12 @@ case $1 in
     cp aws/template/aws.tf.template aws/aws.tf
     sed -i 's/%CLOUDINIT%/"..\/cloud-init-scripts\/installRKE2HA_${count.index}.sh"/g' aws/aws.tf
     sed -i 's/%COUNT%/5/g' aws/aws.tf
-    applyTerraform aws HA
+    applyTofu aws HA
   ;;
   "demo-gpu")
     echo "demo-gpu"
     cp aws/template/aws-demo.tf.template aws/aws.tf
-    applyTerraform aws
+    applyTofu aws
   ;;
   *)
     echo "$0 executed without arg. Please use rke1, rancher, rancher-prime, k3s, k3s-ipv6, rke2 or windows"
